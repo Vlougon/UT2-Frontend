12b0389362035538d5b3c8de8086b55feaa81bc7
Aaaacooooo <acoray06@gmail.com>
2024-01-25 20:18:45 +0000

    Inicio HomePage

--- teleasistencia/src/App.js
+++ teleasistencia/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
-import BeneficiaryForm from './components/BeneficiaryForm';
+//import BeneficiaryForm from './components/BeneficiaryForm';
 import './App.css';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <BeneficiaryForm />
+      {/* <BeneficiaryForm /> */}
+    <HomePage />
     </div>
   );
 }
