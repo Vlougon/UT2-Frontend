bf5ac646ca090d3ab24ca3c5009601317b585cca
Victor <vlougongs@gmail.com>
2024-01-26 17:56:27 +0000

    Updated Images, Layout & BenForm!

---
+++ teleasistencia/src/components/AddresFieldSet.jsx
@@ -0,0 +1,74 @@
+export default function AddresFieldSet() {
+    return (
+        <fieldset>
+            <legend>Domicilio</legend>
+            <div className='row g-3'>
+                <div className='col-md-6'>
+                    <label htmlFor="beneficiaryProvince" className="form-label">Provincia:</label>
+                    <div className='input-group'>
+                        <span className='input-group-text' id='province'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 448 512">
+                                <path d="M128 136c0-22.1-17.9-40-40-40L40 96C17.9 96 0 113.9 0 136l0 48c0 22.1 17.9 40 40 40H88c22.1 0 40-17.9 40-40l0-48zm0 192c0-22.1-17.9-40-40-40H40c-22.1 0-40 17.9-40 40l0 48c0 22.1 17.9 40 40 40H88c22.1 0 40-17.9 40-40V328zm32-192v48c0 22.1 17.9 40 40 40h48c22.1 0 40-17.9 40-40V136c0-22.1-17.9-40-40-40l-48 0c-22.1 0-40 17.9-40 40zM288 328c0-22.1-17.9-40-40-40H200c-22.1 0-40 17.9-40 40l0 48c0 22.1 17.9 40 40 40h48c22.1 0 40-17.9 40-40V328zm32-192v48c0 22.1 17.9 40 40 40h48c22.1 0 40-17.9 40-40V136c0-22.1-17.9-40-40-40l-48 0c-22.1 0-40 17.9-40 40zM448 328c0-22.1-17.9-40-40-40H360c-22.1 0-40 17.9-40 40v48c0 22.1 17.9 40 40 40h48c22.1 0 40-17.9 40-40V328z" />
+                            </svg>
+                        </span>
+                        <select className="form-select" aria-label='beneficiaryProvince' id='beneficiaryProvince' aria-describedby='province'>
+                            <option>Provincia</option>
+                            <option value="1">Santa Cruz</option>
+                            <option value="2">Las Palmas</option>
+                            <option value="3">Iberia</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className='col-md-6'>
+                    <label htmlFor="beneficiaryLocality" className="form-label">Localidad:</label>
+                    <div className='input-group'>
+                        <span className='input-group-text' id='locality'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
+                            </svg>
+                        </span>
+                        <input type="text" className='form-control' id='beneficiaryLocality' placeholder='Localidad' aria-describedby='locality' />
+                    </div>
+                </div>
+
+                <div className='col-md-6'>
+                    <label htmlFor="beneficiaryPostalCode" className="form-label">Código Postal:</label>
+                    <div className='input-group'>
+                        <span className='input-group-text' id='postal-code'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-123" viewBox="0 0 16 16">
+                                <path d="M2.873 11.297V4.142H1.699L0 5.379v1.137l1.64-1.18h.06v5.961zm3.213-5.09v-.063c0-.618.44-1.169 1.196-1.169.676 0 1.174.44 1.174 1.106 0 .624-.42 1.101-.807 1.526L4.99 10.553v.744h4.78v-.99H6.643v-.069L8.41 8.252c.65-.724 1.237-1.332 1.237-2.27C9.646 4.849 8.723 4 7.308 4c-1.573 0-2.36 1.064-2.36 2.15v.057zm6.559 1.883h.786c.823 0 1.374.481 1.379 1.179.01.707-.55 1.216-1.421 1.21-.77-.005-1.326-.419-1.379-.953h-1.095c.042 1.053.938 1.918 2.464 1.918 1.478 0 2.642-.839 2.62-2.144-.02-1.143-.922-1.651-1.551-1.714v-.063c.535-.09 1.347-.66 1.326-1.678-.026-1.053-.933-1.855-2.359-1.845-1.5.005-2.317.88-2.348 1.898h1.116c.032-.498.498-.944 1.206-.944.703 0 1.206.435 1.206 1.07.005.64-.504 1.106-1.2 1.106h-.75z" />
+                            </svg>
+                        </span>
+                        <input type="text" className='form-control' id='beneficiaryPostalCode' placeholder='Código Postal' aria-describedby='postal-code' />
+                        <span className="input-group-text btn btn-orange">Generar C.P.</span>
+                    </div>
+                </div>
+
+                <div className='col-md-3'>
+                    <label htmlFor="beneficiaryStreet" className="form-label">Calle:</label>
+                    <div className='input-group'>
+                        <span className='input-group-text' id='street'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
+                            </svg>
+                        </span>
+                        <input type="text" className='form-control' id='beneficiaryStreet' placeholder='Calle' aria-describedby='street' />
+                    </div>
+                </div>
+
+                <div className='col-md-3'>
+                    <label htmlFor="beneficiaryNumber" className="form-label">Número:</label>
+                    <div className='input-group'>
+                        <span className='input-group-text' id='number'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
+                            </svg>
+                        </span>
+                        <input type="text" className='form-control' id='beneficiaryNumber' placeholder='Número' aria-describedby='number' />
+                    </div>
+                </div>
+            </div>
+        </fieldset>
+    )
+}
