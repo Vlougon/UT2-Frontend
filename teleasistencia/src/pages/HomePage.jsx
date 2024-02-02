711573675291165437069e849c68b48d1526b780
Victor <vlougongs@gmail.com>
2024-02-02 17:34:46 +0000

    Updated Home Page!

--- teleasistencia/src/pages/HomePage.jsx
+++ teleasistencia/src/pages/HomePage.jsx
@@ -3,38 +3,49 @@ import '../styles/HomePage.css';
 
 const HomePage = () => {
     return (
-        <div className="contenedor">
-            {/* Primera columna con 8 botones */}
-            <div className="columna">
-                {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
-                    <button key={i} className="boton">
-                        <a href="tu-enlace-aqui" target="_blank" rel="noopener noreferrer">
-                            <img
-                                src="../images/heart.png"
-                                alt={`Imagen del botón ${i}`}
-                                className="imagen-boton"
-                            />
-                            Botón {i}
-                        </a>
-                    </button>
+        <div id='homePage' className="container-fluid">
+            <div className='row'>
+                <aside className="col-md-3 column">
+                    <div className='row'>
+                        {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
+                            <div key={i} className="d-flex justify-content-evenly columrow">
+                                <a href="/">
+                                    <img
+                                        src="../images/heart.png"
+                                        alt={`Imagen del botón ${i}`}
+                                        className="img-fluid"
+                                        width={40}
+                                        height={40}
+                                    />
+                                    <p>
+                                        Botón {i}
+                                    </p>
+                                </a>
+                                <button>
+                                    -
+                                </button>
+                            </div>
+                        ))}
+                    </div>
+                </aside>
 
-                ))}
-            </div>
-
-            {/* Segunda columna con 4 botones más grandes */}
-            <div className="columna grande">
-                {[9, 10, 11, 12].map((i) => (
-                    <button key={i} className="boton grande">
-                        <a href="tu-enlace-aqui" target="_blank" rel="noopener noreferrer">
-                            <img
-                                src="../images/phone.png"
-                                alt={`Imagen del botón ${i}`}
-                                className="imagen-boton"
-                            />
-                            Botón Grande {i}
-                        </a>
-                    </button>
-                ))}
+                <div className="col-md-9 main">
+                    <div className='row gap-4 justify-content-center align-items-center'>
+                        {[9, 10, 11, 12].map((i) => (
+                            <button key={i} className="col-8 col-sm-5 mainButton">
+                                <a href="tu-enlace-aqui" target="_blank" rel="noopener noreferrer">
+                                    <img
+                                        src="../images/phone.png"
+                                        alt={`Imagen del botón ${i}`}
+                                        className="img-fluid"
+                                        width={300}
+                                        height={300}
+                                    />
+                                </a>
+                            </button>
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     );
