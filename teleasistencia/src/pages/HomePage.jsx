36132dcddeb412fcaf2969cbb68e12d2b357c921
Victor <vlougongs@gmail.com>
2024-02-06 00:34:32 +0000

    Medium Home Page Upgrade!

--- teleasistencia/src/pages/HomePage.jsx
+++ teleasistencia/src/pages/HomePage.jsx
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import BigShortCut from '../components/BigShortCut';
 import ShortCut from '../components/ShortCut';
+import ShortCutModal from '../components/ShortCutModal';
 import '../styles/HomePage.css';
 
-let shortCutsID = 1;
-
 export default function HomePage() {
-    const [shortCuts, setShortCuts] = useState([]);
+    const [shortCuts, setShortCuts] = useState(() => {
+        const storedShortCuts = localStorage.getItem('shortCuts');
+        return storedShortCuts ? JSON.parse(storedShortCuts) : [];
+    });
+    let [shortCutsID, setShortCutsID] = useState(shortCuts.length > 0 ? Math.max(...shortCuts.map(shortcut => shortcut.id)) + 1 : 1);
+
+    useEffect(() => {
+        localStorage.setItem('shortCuts', JSON.stringify(shortCuts));
+    }, [shortCuts]);
 
     const handleShortCutInsert = (element) => {
         const target = element.target;
@@ -25,7 +32,7 @@ export default function HomePage() {
             }
         ])
 
-        shortCutsID++;
+        setShortCutsID(shortCutsID + 1);
     };
 
     const handleShortCutDelete = (element) => {
@@ -49,31 +56,9 @@ export default function HomePage() {
                         <button id='addButton' type="button" data-bs-toggle="modal" data-bs-target="#shortCutModal">
                             +
                         </button>
-
-                        <div className="modal fade" id="shortCutModal" tabIndex="-1" aria-labelledby="shortCutModalLabel" aria-hidden="true">
-                            <div className="modal-dialog">
-                                <div className="modal-content">
-                                    <div className="modal-header">
-                                        <h1 className="modal-title fs-5" id="shortCutModalLabel">Atajos ({shortCuts.length}/7)</h1>
-                                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div className="row modal-body row-cols-3 justify-content-center">
-                                        <img src="../images/addAssistantIcon.png" alt="Añadir Trabajador" title='Añadir Trabajador' prefix='/assistantform' onClick={handleShortCutInsert} />
-                                        <img src="../images/assistantIcon.png" alt="Asistentes" title='Listado de Asistentes' prefix='/assistantlist' onClick={handleShortCutInsert} />
-
-                                        <img src="../images/addBeneficiaryIcon.png" alt="Añadir Beneficiario" title='Añadir Beneficiario' prefix='/beneficiaryform' onClick={handleShortCutInsert} />
-                                        <img src="../images/medicalDataIcon.png" alt="Datos Médicos" title='Datos Médicos' prefix='/medicaldatalist' onClick={handleShortCutInsert} />
-                                        <img src="../images/contactsIcon.png" alt="Contactos" title='Contactos' prefix='/contactlist' onClick={handleShortCutInsert} />
-                                        <img src="../images/calendarIcon.png" alt="Calendario" title='Calendario' prefix='/calendar' onClick={handleShortCutInsert} />
-                                        <img src="../images/unknownCallIcon.png" alt="Llamada Aleatoria" title='Llamada Aleatoria' prefix='/callform' onClick={handleShortCutInsert} />
-                                    </div>
-                                    <div className="modal-footer justify-content-center">
-                                        <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Cerrar</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                     </div>
+
+                    <ShortCutModal currentShortCuts={shortCuts.length} addHandler={handleShortCutInsert} />
                 </aside>
 
                 <div className="col-md-9 main">
