67b5b01fdf19d84684055cebe0abc3e94b6f4a18
Victor <vlougongs@gmail.com>
2024-02-07 01:03:21 +0000

    Medical Data Form Upgraded!

--- teleasistencia/src/components/LocalStructures.jsx
+++ teleasistencia/src/components/LocalStructures.jsx
@@ -1,94 +1,113 @@
-export default function LocalStrucutresFieldSet() {
+export default function LocalStrucutresFieldSet({ outpatientClinic, ambulance, policeStation, fireHouse, emergencyRoom, handler }) {
+    const handleRadioSelection = (element) => {
+        element.target.parentElement.parentElement.querySelector('#' + element.target.name).className = 'invalid-feedback';
+    };
+
     return (
         <fieldset>
             <legend>Edificios Circundantes</legend>
             <div className='row g-3'>
                 <div className='col-md-4'>
-                    <label className="form-label">Hay Ambulatorio en el Municipio:</label>
+                    <label htmlFor="outpatient_clinic_on_town1" className="form-label">Hay Ambulatorio en el Municipio:</label>
                     <div className='input-group'>
                         <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name='ambulantoryOnTown' value="Yes" id="ambulantoryOnTown1" />
-                            <label className="form-check-label" htmlFor="ambulantoryOnTown1">
+                            <input type="radio" name='outpatient_clinic_on_town' value="Yes" id="outpatient_clinic_on_town1" className="form-check-input" checked={outpatientClinic === 'Yes'} onChange={handler} onClick={handleRadioSelection} />
+                            <label className="form-check-label" htmlFor="outpatient_clinic_on_town1">
                                 Si
                             </label>
                         </div>
                         <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name='ambulantoryOnTown' value="No" id="ambulantoryOnTown2" />
-                            <label className="form-check-label" htmlFor="ambulantoryOnTown2">
+                            <input type="radio" name='outpatient_clinic_on_town' value="No" id="outpatient_clinic_on_town2" className="form-check-input" checked={outpatientClinic === 'No'} onChange={handler} onClick={handleRadioSelection} />
+                            <label className="form-check-label" htmlFor="outpatient_clinic_on_town2">
                                 No
                             </label>
+                        </div>
+                        <div id="outpatient_clinic_on_town" className="invalid-feedback">
+                            ¡Es Necesario Seleccionar uno de los Campos!
                         </div>
                     </div>
                 </div>
 
                 <div className='col-md-4'>
-                    <label className="form-label">Hay Ambulancias en el Municipio:</label>
+                    <label htmlFor="ambulance_on_town1" className="form-label">Hay Ambulancias en el Municipio:</label>
                     <div className='input-group'>
                         <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name='ambulancesOnTown' value="Yes" id="ambulancesOnTown1" />
-                            <label className="form-check-label" htmlFor="ambulancesOnTown1">
+                            <input type="radio" name='ambulance_on_town' value="Yes" id="ambulance_on_town1" className="form-check-input" checked={ambulance === 'Yes'} onChange={handler} onClick={handleRadioSelection} />
+                            <label className="form-check-label" htmlFor="ambulance_on_town1">
                                 Si
                             </label>
                         </div>
                         <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name='ambulancesOnTown' value="No" id="ambulancesOnTown2" />
-                            <label className="form-check-label" htmlFor="ambulancesOnTown2">
+                            <input type="radio" name='ambulance_on_town' value="No" id="ambulance_on_town2" className="form-check-input" checked={ambulance === 'No'} onChange={handler} onClick={handleRadioSelection} />
+                            <label className="form-check-label" htmlFor="ambulance_on_town2">
                                 No
                             </label>
+                        </div>
+                        <div id="ambulance_on_town" className="invalid-feedback">
+                            ¡Es Necesario Seleccionar uno de los Campos!
                         </div>
                     </div>
                 </div>
 
                 <div className='col-md-4'>
-                    <label className="form-label">Hay Policía en el Municipio:</label>
+                    <label htmlFor="police_station_on_town1" className="form-label">Hay Policía en el Municipio:</label>
                     <div className='input-group'>
                         <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name='policeOnTown' value="Yes" id="policeOnTown1" />
-                            <label className="form-check-label" htmlFor="policeOnTown1">
+                            <input type="radio" name='police_station_on_town' value="Yes" id="police_station_on_town1" className="form-check-input" checked={policeStation === 'Yes'} onChange={handler} onClick={handleRadioSelection} />
+                            <label className="form-check-label" htmlFor="police_station_on_town1">
                                 Si
                             </label>
                         </div>
                         <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name='policeOnTown' value="No" id="policeOnTown2" />
-                            <label className="form-check-label" htmlFor="policeOnTown2">
+                            <input type="radio" name='police_station_on_town' value="No" id="police_station_on_town2" className="form-check-input" checked={policeStation === 'No'} onChange={handler} onClick={handleRadioSelection} />
+                            <label className="form-check-label" htmlFor="police_station_on_town2">
                                 No
                             </label>
+                        </div>
+                        <div id="police_station_on_town" className="invalid-feedback">
+                            ¡Es Necesario Seleccionar uno de los Campos!
                         </div>
                     </div>
                 </div>
 
                 <div className='col-md-4'>
-                    <label className="form-label">Hay Bomberos en el Municipio:</label>
+                    <label htmlFor="firehouse_on_town1" className="form-label">Hay Bomberos en el Municipio:</label>
                     <div className='input-group'>
                         <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name='fireFightersOnTown' value="Yes" id="fireFightersOnTown1" />
-                            <label className="form-check-label" htmlFor="fireFightersOnTown1">
+                            <input type="radio" name='firehouse_on_town' value="Yes" id="firehouse_on_town1" className="form-check-input" checked={fireHouse === 'Yes'} onChange={handler} onClick={handleRadioSelection} />
+                            <label className="form-check-label" htmlFor="firehouse_on_town1">
                                 Si
                             </label>
                         </div>
                         <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name='fireFightersOnTown' value="No" id="fireFightersOnTown2" />
-                            <label className="form-check-label" htmlFor="fireFightersOnTown2">
+                            <input type="radio" name='firehouse_on_town' value="No" id="firehouse_on_town2" className="form-check-input" checked={fireHouse === 'No'} onChange={handler} onClick={handleRadioSelection} />
+                            <label className="form-check-label" htmlFor="firehouse_on_town2">
                                 No
                             </label>
+                        </div>
+                        <div id="firehouse_on_town" className="invalid-feedback">
+                            ¡Es Necesario Seleccionar uno de los Campos!
                         </div>
                     </div>
                 </div>
 
                 <div className='col-md-4'>
-                    <label className="form-label">Hay Urgencias en el Municipio:</label>
+                    <label htmlFor="emergency_room_on_town1" className="form-label">Hay Urgencias en el Municipio:</label>
                     <div className='input-group'>
                         <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name='emergencyRoomOnTown' value="Yes" id="emergencyRoomOnTown1" />
-                            <label className="form-check-label" htmlFor="emergencyRoomOnTown1">
+                            <input type="radio" name='emergency_room_on_town' value="Yes" id="emergency_room_on_town1" className="form-check-input" checked={emergencyRoom === 'Yes'} onChange={handler} onClick={handleRadioSelection} />
+                            <label className="form-check-label" htmlFor="emergency_room_on_town1">
                                 Si
                             </label>
                         </div>
                         <div className="form-check form-check-inline">
-                            <input className="form-check-input" type="radio" name='emergencyRoomOnTown' value="No" id="emergencyRoomOnTown2" />
-                            <label className="form-check-label" htmlFor="emergencyRoomOnTown2">
+                            <input type="radio" name='emergency_room_on_town' value="No" id="emergency_room_on_town2" className="form-check-input" checked={emergencyRoom === 'No'} onChange={handler} onClick={handleRadioSelection} />
+                            <label className="form-check-label" htmlFor="emergency_room_on_town2">
                                 No
                             </label>
+                        </div>
+                        <div id="emergency_room_on_town" className="invalid-feedback">
+                            ¡Es Necesario Seleccionar uno de los Campos!
                         </div>
                     </div>
                 </div>
--- teleasistencia/src/components/MedicinesFieldSet.jsx
+++ teleasistencia/src/components/MedicinesFieldSet.jsx
@@ -1,77 +1,92 @@
-export default function MedicinesFieldSet() {
+export default function MedicinesFieldSet({ medMorn, medNoon, medNight, hourMorn, hourNoon, HourNight, handler }) {
+    const handleHourInput = (element) => {
+        element.target.className = 'form-control';
+        element.target.previousElementSibling.className = 'input-group-text';
+        element.target.nextElementSibling.className = 'invalid-feedback';
+    }
+
     return (
         <fieldset>
             <legend>Medicinas y Horarios de Llamada</legend>
             <div className='row g-3'>
                 <div className='col-md-4'>
-                    <label htmlFor="morningMedication" className="form-label">Medicación por la Mañana:</label>
+                    <label htmlFor="morning_medication" className="form-label">Medicación por la Mañana:</label>
                     <div className='input-group'>
-                        <span className='input-group-text' id='locality'>
+                        <span className='input-group-text' id='morningMedication'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-capsule" viewBox="0 0 16 16">
                                 <path d="M1.828 8.9 8.9 1.827a4 4 0 1 1 5.657 5.657l-7.07 7.071A4 4 0 1 1 1.827 8.9Zm9.128.771 2.893-2.893a3 3 0 1 0-4.243-4.242L6.713 5.429z" />
                             </svg>
                         </span>
-                        <textarea className="form-control" id="morningMedication"></textarea>
+                        <textarea id="morning_medication" name="morning_medication" value={medMorn} className="form-control" onChange={handler}></textarea>
                     </div>
                 </div>
 
                 <div className='col-md-4'>
-                    <label htmlFor="noonMedication" className="form-label">Medicación por la Tarde:</label>
+                    <label htmlFor="afternoon_medication" className="form-label">Medicación por la Tarde:</label>
                     <div className='input-group'>
-                        <span className='input-group-text' id='locality'>
+                        <span className='input-group-text' id='noonMedication'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-capsule" viewBox="0 0 16 16">
                                 <path d="M1.828 8.9 8.9 1.827a4 4 0 1 1 5.657 5.657l-7.07 7.071A4 4 0 1 1 1.827 8.9Zm9.128.771 2.893-2.893a3 3 0 1 0-4.243-4.242L6.713 5.429z" />
                             </svg>
                         </span>
-                        <textarea className="form-control" id="noonMedication"></textarea>
+                        <textarea id="afternoon_medication" name="afternoon_medication" value={medNoon} className="form-control" onChange={handler}></textarea>
                     </div>
                 </div>
 
                 <div className='col-md-4'>
-                    <label htmlFor="nightMedication" className="form-label">Medicación por la Noche:</label>
+                    <label htmlFor="night_medication" className="form-label">Medicación por la Noche:</label>
                     <div className='input-group'>
-                        <span className='input-group-text' id='locality'>
+                        <span className='input-group-text' id='nightMedication'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-capsule" viewBox="0 0 16 16">
                                 <path d="M1.828 8.9 8.9 1.827a4 4 0 1 1 5.657 5.657l-7.07 7.071A4 4 0 1 1 1.827 8.9Zm9.128.771 2.893-2.893a3 3 0 1 0-4.243-4.242L6.713 5.429z" />
                             </svg>
                         </span>
-                        <textarea className="form-control" id="nightMedication"></textarea>
+                        <textarea id="night_medication" name="night_medication" value={medNight} className="form-control" onChange={handler}></textarea>
                     </div>
                 </div>
 
                 <div className='col-md-4'>
-                    <label htmlFor="preferableHourMorning" className="form-label">Hora preferente de llamada (Mañana):</label>
-                    <div className='input-group'>
-                        <span className='input-group-text' id='locality'>
+                    <label htmlFor="preferent_morning_calls_hour" className="form-label">Hora preferente de llamada (Mañana):</label>
+                    <div className='input-group has-validation'>
+                        <span className='input-group-text' id='preferableHourMorning'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clock-fill" viewBox="0 0 16 16">
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
                             </svg>
                         </span>
-                        <input type="text" className='form-control' id="preferableHourMorning" />
+                        <input type="time" id="preferent_morning_calls_hour" name="preferent_morning_calls_hour" value={hourMorn} className='form-control' onChange={handler} onClick={handleHourInput} required />
+                        <div className="invalid-feedback">
+                            ¡Introduza una Hora adecuada para la Mañana!
+                        </div>
                     </div>
                 </div>
 
                 <div className='col-md-4'>
-                    <label htmlFor="preferableHourNoon" className="form-label">Hora preferente de llamada (Tarde):</label>
-                    <div className='input-group'>
-                        <span className='input-group-text' id='locality'>
+                    <label htmlFor="preferent_afternoon_calls_hour" className="form-label">Hora preferente de llamada (Tarde):</label>
+                    <div className='input-group has-validation'>
+                        <span className='input-group-text' id='preferableHourNoon'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clock-fill" viewBox="0 0 16 16">
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
                             </svg>
                         </span>
-                        <input type="text" className='form-control' id="preferableHourNoon" />
+                        <input type="time" id="preferent_afternoon_calls_hour" name="preferent_afternoon_calls_hour" value={hourNoon} className='form-control' onChange={handler} onClick={handleHourInput} required />
+                        <div className="invalid-feedback">
+                            ¡Introduza una Hora adecuada para la Tarde!
+                        </div>
                     </div>
                 </div>
 
                 <div className='col-md-4'>
-                    <label htmlFor="preferableHourNight" className="form-label">Hora preferente de llamada (Noche):</label>
-                    <div className='input-group'>
-                        <span className='input-group-text' id='locality'>
+                    <label htmlFor="preferent_night_calls_hour" className="form-label">Hora preferente de llamada (Noche):</label>
+                    <div className='input-group has-validation'>
+                        <span className='input-group-text' id='preferableHourNight'>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clock-fill" viewBox="0 0 16 16">
                                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
                             </svg>
                         </span>
-                        <input type="text" className='form-control' id="preferableHourNight" />
+                        <input type="time" id="preferent_night_calls_hour" name="preferent_night_calls_hour" value={HourNight} className='form-control' onChange={handler} onClick={handleHourInput} required />
+                        <div className="invalid-feedback">
+                            ¡Introduza una Hora adecuada para la Noche!
+                        </div>
                     </div>
                 </div>
             </div>
