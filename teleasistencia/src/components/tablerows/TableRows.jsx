35c0ddcdfb41edbea4cb829db4ca92323b5a06ca
Victor <vlougongs@gmail.com>
2024-02-12 20:17:22 +0000

    Added Confirmation Modal!

--- teleasistencia/src/components/tablerows/TableRows.jsx
+++ teleasistencia/src/components/tablerows/TableRows.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import ConfirmationModal from '../../components/modals/ConfirmationModal';
 
 export default function TableRows({ columns, list, dataArray }) {
     const tableRows = [];
@@ -33,11 +34,13 @@ export default function TableRows({ columns, list, dataArray }) {
                                 </Link>
                             </td>
                             <td>
-                                <button className="btn btn-outline-danger">
+                                <button className="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target={'#userModal' + user.id}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
                                         <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
                                     </svg>
                                 </button>
+
+                                <ConfirmationModal modalID={'userModal' + user.id} modalOn={'Asistente'} elementID={user.id} />
                             </td>
                         </tr>
                     )
@@ -74,11 +77,13 @@ export default function TableRows({ columns, list, dataArray }) {
                                 </Link>
                             </td>
                             <td>
-                                <button className="btn btn-outline-danger">
+                                <button className="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target={'#beneficiaryModal' + beneficiary.id}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
                                         <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
                                     </svg>
                                 </button>
+
+                                <ConfirmationModal modalID={'beneficiaryModal' + beneficiary.id} modalOn={'Beneficiario'} elementID={beneficiary.id} />
                             </td>
                         </tr>
                     )
@@ -98,11 +103,13 @@ export default function TableRows({ columns, list, dataArray }) {
                                 </Link>
                             </td>
                             <td>
-                                <button className="btn btn-outline-danger">
+                                <button className="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target={'#medicalModal' + medicalData.id}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
                                         <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
                                     </svg>
                                 </button>
+
+                                <ConfirmationModal modalID={'medicalModal' + medicalData.id} modalOn={'Informe Médico'} elementID={medicalData.id} />
                             </td>
                         </tr>
                     )
@@ -123,11 +130,13 @@ export default function TableRows({ columns, list, dataArray }) {
                                 </Link>
                             </td>
                             <td>
-                                <button className="btn btn-outline-danger">
+                                <button className="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target={'#contactModal' + contact.id}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
                                         <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
                                     </svg>
                                 </button>
+
+                                <ConfirmationModal modalID={'contactModal' + contact.id} modalOn={'Contacto'} elementID={contact.id} />
                             </td>
                         </tr>
                     )
